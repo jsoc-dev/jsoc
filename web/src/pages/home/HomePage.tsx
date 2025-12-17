b936353e9248f3f0866cafe0c4fbe0991abdf6e2
amanx5 <110730645+amanx5@users.noreply.github.com>
2025-12-18 00:01:56 +0530

    [docsite] HomePage UI refinement

--- web/src/pages/home/HomePage.tsx
+++ web/src/pages/home/HomePage.tsx
@@ -1,38 +1,61 @@
 import { Link } from 'react-router';
 
-export function HomePage() {
-	const features = [
-		['Dynamic', 'No fixed rows, columns configuration.'],
-		['JSON-driven', 'Auto configured based on JSON data.'],
-		['Adaptive UI', 'Powered by popular UI libraries.'],
-	];
+const features = [
+	['Dynamic', 'No fixed rows or column configuration.'],
+	['JSON-Driven', 'Auto configured based on JSON data.'],
+	['Adaptive UI', 'Powered by popular UI libraries.'],
+];
 
+const links = [
+	['Playground', '/demos/grid/react'],
+	['Documentation', '/docs/grid/react'],
+];
+
+export function HomePage() {
 	return (
 		<div>
-			{/* tagline and frameworks section */}
-			<section className='text-center mt-20'>
-				{/* tagline */}
-				<div>
-					<h1 className='text-[clamp(2rem,5vw,4rem)] mb-5'>
-						Introducing <b>JSOC</b> Grid
-					</h1>
-					<p className='text-[clamp(1rem,3vw,1.5rem)]'>
-						Dynamic, JSON-driven and adaptive UI grid.
-					</p>
-					<div className='mt-10'>
-						<Link className='link-button' to='/docs/grid/react'>
-							Documentation ↗︎
+			{/* Hero section */}
+			<section className='text-center mt-24'>
+				<h1 className='text-[clamp(2rem,5vw,4rem)] mb-5 font-semibold'>
+					<span>Introducing </span>
+					<span className='font-bold tracking-wide'>JSOC</span> Grid
+				</h1>
+
+				<p className='text-[clamp(1rem,3vw,1.5rem)] text-gray-700'>
+					Dynamic, JSON-driven and adaptive UI grid.
+				</p>
+
+				<div className='flex flex-wrap justify-center gap-4 mt-12'>
+					{links.map(([text, path], index) => (
+						<Link
+							className={'link-button border border-black text-[clamp(0.9rem,3vw,1rem)]'}
+							key={index}
+							to={path}
+							title={text}
+						>
+							{text} <span aria-hidden>↗︎</span>
 						</Link>
-					</div>
+					))}
 				</div>
 			</section>
 
-			<section className='mt-20 p-y-6 flex flex-col space-y-10 ' >
-				{features.map(([featureName, featureBrief], index) => (
-					<div key={index} className='inline-flex flex-col border-black border-2 rounded-2xl p-6 border-dashed'>
-						<h1 className='text-[clamp(1.5rem,5vw,2rem)]'>{featureName}</h1>
-						<p className='text-[clamp(0.7rem,3vw,1rem)]'>
-							{featureBrief}
+			{/* Features */}
+			<section className='mt-14 py-6 flex flex-col space-y-12 items-center'>
+				{features.map(([title, description], index) => (
+					<div
+						key={index}
+						className='inline-flex items-center border-2 border-dashed border-black rounded-2xl px-8 py-6 max-w-3xl w-full'
+					>
+						<div className='w-[45%] text-right pr-4'>
+							<h2 className='break-words text-[clamp(1.1rem,4vw,2rem)] font-semibold'>
+								{title}
+							</h2>
+						</div>
+
+						<span className='border-r border-gray-300 self-stretch'></span>
+
+						<p className='break-words w-[55%] pl-4 text-[clamp(0.8rem,3vw,1rem)] text-gray-700'>
+							{description}
 						</p>
 					</div>
 				))}
