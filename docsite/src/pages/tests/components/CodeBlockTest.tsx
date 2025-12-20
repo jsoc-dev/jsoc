0085910aa76e53eab55ae3a19e972f45a3bb51c8
amanx5 <110730645+amanx5@users.noreply.github.com>
2025-12-21 04:49:06 +0530

    [docsite] Word count support in getSampleLines

--- docsite/src/pages/tests/components/CodeBlockTest.tsx
+++ docsite/src/pages/tests/components/CodeBlockTest.tsx
@@ -4,6 +4,21 @@ import { CodeInline } from '../../../components/CodeInline';
 import { getSampleLines } from '../../../utils/lorem';
 
 export function CodeBlockTest() {
+	const wordCounts = {
+		small: {
+			min: 3,
+			max: 6,
+		},
+		large: {
+			min: 30,
+			max: 40,
+		},
+		random: {
+			min: 1,
+			max: 50,
+		},
+	};
+
 	return (
 		<>
 			<Section
@@ -15,18 +30,39 @@ export function CodeBlockTest() {
 				}
 			></Section>
 
-			<Section title='Single line'>
-				<CodeBlock lang='cmd'>{getSampleLines(1)}</CodeBlock>
+			<Section title='Single Small line'>
+				<CodeBlock lang='cmd'>{getSampleLines(1, wordCounts.small.min, wordCounts.small.max)}</CodeBlock>
 			</Section>
 
+			<Section title='Single Small highlighted line '>
+				<CodeBlock lang='cmd' highlightLines={[1]}>
+					{getSampleLines(1, wordCounts.small.min, wordCounts.small.max)}
+				</CodeBlock>
+			</Section>
+
+			<Section title='Single Large line'>
+				<CodeBlock lang='cmd'>{getSampleLines(1, wordCounts.large.min, wordCounts.large.max)}</CodeBlock>
+			</Section>
+
+			<Section title='Single Large highlighted line '>
+				<CodeBlock lang='cmd' highlightLines={[1]}>
+					{getSampleLines(1, wordCounts.large.min, wordCounts.large.max)}
+				</CodeBlock>
+			</Section>
+
+			<Section title='Ten small lines including highlighted'>
+				<CodeBlock lang='cmd' highlightLines={[5]}>
+					{getSampleLines(10, wordCounts.small.min, wordCounts.small.max)}
+				</CodeBlock>
+			</Section>
 			<Section title='Ten lines including highlighted'>
-				<CodeBlock lang='cmd' highlightLines={[3, 6]}>
-					{getSampleLines(10)}
+				<CodeBlock lang='cmd' highlightLines={[10]}>
+					{getSampleLines(10, wordCounts.random.min, wordCounts.random.max)}
 				</CodeBlock>
 			</Section>
 
 			<Section title='Hundred lines'>
-				<CodeBlock lang='cmd'>{getSampleLines(100)}</CodeBlock>
+				<CodeBlock lang='cmd'>{getSampleLines(100, wordCounts.random.min, wordCounts.random.max)}</CodeBlock>
 			</Section>
 		</>
 	);
--- docsite/src/utils/lorem.ts
+++ docsite/src/utils/lorem.ts
@@ -1,21 +1,31 @@
-import { range } from "@jsoc/core/utils";
-
-const line =
-	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia accusamus, reprehenderit, nobis itaque dolorem fugit ipsa vel minima natus enim quis nesciunt. Fugiat, sed? Iure ex quam suscipit est temporibus?';
+import { range } from '@jsoc/core/utils';
 
 function shuffle<T>(arr: T[]) {
 	return [...arr].sort(() => Math.random() - 0.5);
 }
 
-export function getSampleLines(count: number) {
-	if (count === 1) return line;
+export function getSampleLines(
+	count: number,
+	minWordCount: number = 1,
+	maxWordCount: number = 10
+) {
+	const line =
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia accusamus, reprehenderit, nobis itaque dolorem fugit ipsa vel minima natus enim quis nesciunt. Fugiat, sed? Iure ex quam suscipit est temporibus?';
 
 	const words = line.split(' ');
 
 	return range(1, count)
 		.map(() =>
 			shuffle(words)
-				.slice(0, Math.floor(Math.random() * words.length) + 3)
+				.slice(
+					0,
+					Math.min(
+						Math.floor(
+							Math.random() * (maxWordCount - minWordCount + 1)
+						) + minWordCount,
+						words.length
+					)
+				)
 				.join(' ')
 		)
 		.join('\n');
