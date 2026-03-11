5d65f49cda228533b8f4c94fb22db147e6766cb8
amanx5 <110730645+amanx5@users.noreply.github.com>
2026-03-11 23:10:47 +0530

    [react/grid] Remove MUI DataGrid apiRef and context
    Previously, JsocGrid was wrapped in a context containing the MUI DataGrid API ref, allowing consumers to call imperative methods.
    
    That API caused React warnings “Can't perform a React state update on a component that hasn't mounted yet…” during strict-mode rendering
    because it looks like `useGridApiRef` internally schedules asynchronous updates before the component has mounted.
    
    The context is currently unused within the repository, so we remove it to eliminate the warning.
    If there is a future need to expose the API, we need to re‑introduce the context alongside proper mounting guards.

--- packages/react/src/grid/adapters/mui/JsocGridMui.tsx
+++ packages/react/src/grid/adapters/mui/JsocGridMui.tsx
@@ -1,22 +1,20 @@
-import { JsocGridContext } from '../../JsocGrid';
-import { COLUMN_FACTORY_MUI } from './';
-import { JsocGridError } from '@jsoc/core/errors';
-import { SubsetKeysOf } from '@jsoc/core/utils';
+import { JsocGridContext } from "../../JsocGrid";
+import { COLUMN_FACTORY_MUI } from "./";
+import { JsocGridError } from "@jsoc/core/errors";
+import { SubsetKeysOf } from "@jsoc/core/utils";
 import {
-	generateColumns,
-	CustomColumnFactory,
-	GridId,
-	searchGridSchema,
-} from '@jsoc/core/grid';
-import { createContext, RefObject, useContext, useMemo } from 'react';
+  generateColumns,
+  CustomColumnFactory,
+  GridId,
+  searchGridSchema,
+} from "@jsoc/core/grid";
+import { useContext, useMemo } from "react";
 import {
-	DataGrid,
-	DataGridProps,
-	GridColDef,
-	useGridApiRef,
-	type GridRowIdGetter,
-} from '@mui/x-data-grid';
-import { GridApiCommunity } from '@mui/x-data-grid/internals';
+  DataGrid,
+  DataGridProps,
+  GridColDef,
+  type GridRowIdGetter,
+} from "@mui/x-data-grid";
 
 //#region props
 /**
@@ -24,40 +22,41 @@ import { GridApiCommunity } from '@mui/x-data-grid/internals';
  * These props are not part of DataGridProps as they don't exist in MUI DataGrid
  */
 export type JsocGridMuiCustomProps = {
-	/**
-	 * Name for the root grid. This is used by the default navigator to indicate whether
-	 * root grid is active or not.
-	 * - Note: This prop is managed by `JsocGrid` and always has a value even if you don't
-	 * 	provide any value.
-	 */
-	gridId?: GridId;
-	/**
-	 * Object containing 0 or more custom `ColumnDefinitionProvider`s to override the
-	 * corresponding default `ColumnDefinitionProvider`s for the `ColumnDataType`.
-	 */
-	columnFactory?: CustomColumnFactory<GridColDef>;
+  /**
+   * Name for the root grid. This is used by the default navigator to indicate whether
+   * root grid is active or not.
+   * - Note: This prop is managed by `JsocGrid` and always has a value even if you don't
+   * 	provide any value.
+   */
+  gridId?: GridId;
+  /**
+   * Object containing 0 or more custom `ColumnDefinitionProvider`s to override the
+   * corresponding default `ColumnDefinitionProvider`s for the `ColumnDataType`.
+   */
+  columnFactory?: CustomColumnFactory<GridColDef>;
 };
 
 /**
  * Props that are dynamically injected by the adapter `JsocGridMui` into the `DataGrid`
  * These props are subset of `DataGridProps` but not required to be supplied by the consumer.
- * If provided, will be overridden by the dynamically injected values.
+ * If provided, will be overridden by the dynamically injected values. (The API reference
+ * used to be injected here as well but was removed because it was causing React warnings.)
  */
 export type JsocGridMuiInjectedPropNames = SubsetKeysOf<
-	DataGridProps,
-	'apiRef' | 'rows' | 'columns' | 'getRowId'
+  DataGridProps,
+  "rows" | "columns" | "getRowId"
 >;
 export type JsocGridMuiInjectedProps = Pick<
-	DataGridProps,
-	JsocGridMuiInjectedPropNames
+  DataGridProps,
+  JsocGridMuiInjectedPropNames
 >;
 
 /**
  * The public adapter component props that will be supplied by the consumer.
  */
 export type JsocGridMuiProps = {
-	native?: Omit<DataGridProps, JsocGridMuiInjectedPropNames>;
-	custom?: JsocGridMuiCustomProps;
+  native?: Omit<DataGridProps, JsocGridMuiInjectedPropNames>;
+  custom?: JsocGridMuiCustomProps;
 };
 //#endregion
 
@@ -65,51 +64,34 @@ export type JsocGridMuiProps = {
  * Adapter component for MUI DataGrid
  */
 export function JsocGridMui({ native = {}, custom = {} }: JsocGridMuiProps) {
-	if (!custom.gridId) {
-		throw new JsocGridError(
-			"An internal error occured. JsocGrid didn't provide the gridId to the adapter.",
-		);
-	}
+  if (!custom.gridId) {
+    throw new JsocGridError(
+      "An internal error occured. JsocGrid didn't provide the gridId to the adapter.",
+    );
+  }
 
-	const { gridSchemaStore } = useContext(JsocGridContext);
-	const apiRef = useGridApiRef();
-	const { gridSchema } = searchGridSchema(gridSchemaStore, custom.gridId);
-	const { gridRows, gridIdColumnKey } = gridSchema;
-	const getRowId: GridRowIdGetter = (row) => row[gridIdColumnKey];
+  const { gridSchemaStore } = useContext(JsocGridContext);
+  const { gridSchema } = searchGridSchema(gridSchemaStore, custom.gridId);
+  const { gridRows, gridIdColumnKey } = gridSchema;
+  const getRowId: GridRowIdGetter = (row) => row[gridIdColumnKey];
 
-	/**
-	 * Memoising col defs to persist column states. Also suggested at official docs:
-	 * https://mui.com/x/react-data-grid/column-definition/#:~:text=The%20columns%20prop%20should,function%20or%20memoize%20it.
-	 */
-	const columns = useMemo(
-		() =>
-			generateColumns(
-				gridSchema,
-				COLUMN_FACTORY_MUI,
-				custom.columnFactory,
-			),
-		// re-generate only when current gridSchema or columnFactory prop changes
-		[gridSchema, custom.columnFactory],
-	);
+  /**
+   * Memoising col defs to persist column states. Also suggested at official docs:
+   * https://mui.com/x/react-data-grid/column-definition/#:~:text=The%20columns%20prop%20should,function%20or%20memoize%20it.
+   */
+  const columns = useMemo(
+    () => generateColumns(gridSchema, COLUMN_FACTORY_MUI, custom.columnFactory),
+    // re-generate only when current gridSchema or columnFactory prop changes
+    [gridSchema, custom.columnFactory],
+  );
 
-
-	return (
-		<JsocGridMuiContext.Provider value={{ apiRef }}>
-			<DataGrid
-				{...native}
-				apiRef={apiRef}
-				rows={gridRows}
-				getRowId={getRowId}
-				columns={columns}
-			/>
-		</JsocGridMuiContext.Provider>
-	);
+  return (
+    <DataGrid
+      {...native}
+      rows={gridRows}
+      getRowId={getRowId}
+      columns={columns}
+    />
+  );
 }
 
-export type DataGridCommunityApiRef = RefObject<GridApiCommunity | null>;
-export type JsocGridMuiContextValue = {
-	apiRef: DataGridCommunityApiRef;
-};
-export const JsocGridMuiContext = createContext<JsocGridMuiContextValue>({
-	apiRef: { current: null },
-});
